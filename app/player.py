1e1b3d24e81e4f8f2bc939e5bddff8bdca788037
habib <habibadeku@gmail.com>
2025-02-15 11:35:22 +0100

    refactor: add to queue

--- app/player.py
+++ app/player.py
@@ -27,6 +27,9 @@ class Player:
         self.media_list_player = self.instance.media_list_player_new()  # Create a MediaListPlayer
         self.media_list_player.set_media_list(self.media_list)  # Set the MediaList
 
+        # Track the length of the MediaList manually
+        self.media_list_length = 0
+
         # Attach event listeners
         self.event_manager = self.media_list_player.get_media_player().event_manager()
         self.event_manager.event_attach(
@@ -50,17 +53,54 @@ class Player:
             self.play_track_at_offset(track)
 
             # Preload the next 5 tracks
-            # self.preload_next_tracks(5)
+            self.preload_next_tracks(5)
         except Exception as e:
             logger.error(f"Error in on_media_player_playing media: {e}")
+
+    def on_media_player_end_reached(self, event):
+        """Callback when playback ends."""
+        try:
+            logger.info("Playback ended. The event is: %s" % event)
+            self.current_track_index += 1
+            if self.current_track_index < self.playlist_length:
+                self.media_list_player.play_item_at_index(self.current_track_index)
+            else:
+                logger.info("Playlist finished.")
+        except Exception as e:
+            logger.error(f"Error in on_media_player_end_reached: {e}")
+
+    def add_track_to_queue(self, track):
+        """Add a track to the player queue."""
+        try:
+            track_md5 = track.get("md5")
+            track_id = track.get("id")
+            track_url = (f"{NORMALIZED_CDN}/{track.get('id')}" if track.get("type") == "Track"
+                        else track.get("url"))
+            logger.info(f"Add track to queue, md5, {track_md5} added to queue.")
+
+            track_path = self.download_track(track_url, track_id, track_md5)
+
+            if track_path:
+                media = self.instance.media_new(track_path)
+                self.media_list.add_media(media)  # Add media to the MediaList
+                self.media_list_length += 1  # Update the MediaList length
+                self.playlist.append({"media": media, "track": track})  # Store track metadata
+                self.playlist_length += 1
+                logger.info(f"Track {track_id} added to queue.")
+            else:
+                logger.error(f"Failed to add track {track_id} to queue.")
+
+        except Exception as e:
+            logger.error(f"Error adding track to queue: {e}")
 
     def preload_next_tracks(self, count):
         """Download and add the next `count` tracks to the playlist."""
         try:
-            next_index = self.current_track_index + 1
-            end_index = min(next_index + count, len(self.playlist))
+            # Start from the last track in the player queue
+            start_index = self.media_list_length
+            end_index = min(start_index + count, len(self.playlist))
 
-            for i in range(next_index, end_index):
+            for i in range(start_index, end_index):
                 if i >= len(self.playlist):
                     break  # No more tracks to preload
 
@@ -76,24 +116,13 @@ class Player:
                 if track_path:
                     media = self.instance.media_new(track_path)
                     self.media_list.add_media(media)  # Add media to the MediaList
+                    self.media_list_length += 1  # Update the MediaList length
                     logger.info(f"Preloaded track {track_id} into the playlist.")
                 else:
                     logger.error(f"Failed to preload track {track_id}.")
 
         except Exception as e:
-            logger.error(f"Error preloading next tracks: {e}")
-
-    def on_media_player_end_reached(self, event):
-        """Callback when playback ends."""
-        try:
-            logger.info("Playback ended. The event is: %s" % event)
-            self.current_track_index += 1
-            if self.current_track_index < self.playlist_length:
-                self.media_list_player.play_item_at_index(self.current_track_index)
-            else:
-                logger.info("Playlist finished.")
-        except Exception as e:
-            logger.error(f"Error in on_media_player_end_reached: {e}")
+            logger.error(f"Error preloading next tracks: {e}")            
 
     def download_track(self, url, track_id, track_md5):
         """Download a track and save it to the cache directory."""
@@ -117,29 +146,6 @@ class Player:
         except Exception as e:
             logger.error(f"Failed to download track {track_id}: {e}")
             return None
-
-    def add_track_to_queue(self, track):
-        """Add a track to the player queue."""
-        try:
-            track_md5 = track.get("md5")
-            track_id = track.get("id")
-            track_url = (f"{NORMALIZED_CDN}/{track.get('id')}" if track.get("type") == "Track"
-                        else track.get("url"))
-            logger.info(f"Add track to queue, md5, {track_md5} added to queue.")
-
-            track_path = self.download_track(track_url, track_id, track_md5)
-
-            if track_path:
-                media = self.instance.media_new(track_path)
-                self.media_list.add_media(media)  # Add media to the MediaList
-                self.playlist.append({"media": media, "track": track})  # Store track metadata
-                self.playlist_length += 1
-                logger.info(f"Track {track_id} added to queue.")
-            else:
-                logger.error(f"Failed to add track {track_id} to queue.")
-
-        except Exception as e:
-            logger.error(f"Error adding track to queue: {e}")
 
     def play_track_at_offset(self, track):
         """Play a track from a specific offset."""
